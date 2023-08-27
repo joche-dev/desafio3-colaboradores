28b9a280797b90a12a273da5938d24e80601a587
Jose Albornoz <prueba@outlook.cl>
2023-08-27 02:14:34 -0400

    correcion de id en agregar nuevo colaborador, limpieza de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import {BaseColaboradores} from './BaseColaboradores';
+import { BaseColaboradores } from './BaseColaboradores';
 import Buscador from './componentes/Buscador';
 import Listado from './componentes/Listado';
 import Formulario from './componentes/Formulario';
@@ -9,25 +9,9 @@ import Alert from './componentes/Alert/Alert';
 import { useState } from 'react';
 
 function App() {
-
-  /** 
-  const [searchTerm, setSearchTerm] = useState('');
-  */
-
   const [baseColaboradores, setBaseColaboradores] = useState(BaseColaboradores);
-  const [colaboradoresFiltrado, setColaboradoresFiltrado] = useState(baseColaboradores)
-
-
-
-  /**
-  const filteredColaboradores = baseColaboradores.filter(colaborador =>
-    colaborador.nombre.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-
-  const handleSearch = (term) => {
-    setSearchTerm(term);
-  };
-  */
+  const [colaboradoresFiltrado, setColaboradoresFiltrado] =
+    useState(baseColaboradores);
 
   const [alert, setAlert] = useState({
     texto: '',
@@ -36,19 +20,32 @@ function App() {
   });
 
   const addAlert = (newAlert) => {
-    setAlert (newAlert)
-  }
+    setAlert(newAlert);
+  };
 
   return (
     <>
       <h1 className="my-4">Lista de Colaboradores</h1>
-      <Buscador data={baseColaboradores} dataFilter={setColaboradoresFiltrado}/>
+      <Buscador
+        data={baseColaboradores}
+        dataFilter={setColaboradoresFiltrado}
+      />
       <div className="row row-cols-2 m-0">
-        <Listado data={baseColaboradores} setData={setBaseColaboradores} dataFilter={colaboradoresFiltrado} setDataFilter={setColaboradoresFiltrado}/>
-      </div>
-      <div className='div-form-alert'>
-        <Formulario className="formulario" addAlert={addAlert} setData={setBaseColaboradores} data={baseColaboradores}/>
-        <Alert className="alert" alerta={alert}/>
+        <Listado
+          data={baseColaboradores}
+          setData={setBaseColaboradores}
+          dataFilter={colaboradoresFiltrado}
+          setDataFilter={setColaboradoresFiltrado}
+        />
+        <Formulario
+          className="formulario"
+          addAlert={addAlert}
+          data={baseColaboradores}
+          setData={setBaseColaboradores}
+          dataFilter={colaboradoresFiltrado}
+          setDataFilter={setColaboradoresFiltrado}
+        />
+        <Alert className="alert" alerta={alert} />
       </div>
     </>
   );
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,45 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 import { useState, useEffect } from 'react';
 
-export default function Formulario({ addAlert, setData, data }) {
-
+export default function Formulario({
+  addAlert,
+  setData,
+  data,
+  setDataFilter,
+  dataFilter,
+}) {
   const [datosColaborador, setDatosColaborador] = useState({
-    nombre: "",
-    correo: "",
-    edad: "",
-    cargo: "",
-    telefono: ""
+    nombre: '',
+    correo: '',
+    edad: '',
+    cargo: '',
+    telefono: '',
   });
 
   function handlerInputs(e) {
-
-    if (e.target.id === "inputNombre") {
+    if (e.target.id === 'inputNombre') {
       setDatosColaborador({ ...datosColaborador, nombre: e.target.value });
     }
 
-    if (e.target.id === "inputEmail") {
+    if (e.target.id === 'inputEmail') {
       setDatosColaborador({ ...datosColaborador, correo: e.target.value });
     }
 
-    if (e.target.id === "inputEdad") {
+    if (e.target.id === 'inputEdad') {
       setDatosColaborador({ ...datosColaborador, edad: e.target.value });
     }
-    if (e.target.id === "inputCargo") {
+    if (e.target.id === 'inputCargo') {
       setDatosColaborador({ ...datosColaborador, cargo: e.target.value });
     }
-    if (e.target.id === "inputTelefono") {
+    if (e.target.id === 'inputTelefono') {
       setDatosColaborador({ ...datosColaborador, telefono: e.target.value });
     }
-
-    //setDatosColaborador({ ...datosColaborador, id: (parseInt(data[data.length-1].id)+1).toString()});
-
-    console.log(datosColaborador);
-
   }
 
   function validandoDatos(e) {
-
     e.preventDefault();
 
     const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -75,75 +73,97 @@ export default function Formulario({ addAlert, setData, data }) {
         texto: 'Colaborador Agregado',
         tipo: 'alert-success',
         estado: true,
-      }
-      );
-
-      const newId = (parseInt(data[data.length - 1].id) + 1).toString();
-
-      //datosColaborador.id = (parseInt(data[data.length-1].id)+1).toString()
-
-      //setData([...data,datosColaborador])
-
-      setData([...data, { ...datosColaborador, id: newId }]);
-
-      setDatosColaborador({
-        nombre: "",
-        correo: "",
-        edad: "",
-        cargo: "",
-        telefono: ""
       });
 
+      const newId = data.length
+        ? (parseInt(data[data.length - 1].id) + 1).toString()
+        : '0';
+
+      setData([...data, { ...datosColaborador, id: newId }]);
+      setDataFilter([...dataFilter, { ...datosColaborador, id: newId }]);
+
+      setDatosColaborador({
+        nombre: '',
+        correo: '',
+        edad: '',
+        cargo: '',
+        telefono: '',
+      });
     }
-
-    //console.log(addAlert);
-
-    //console.log(data);
-
   }
 
   useEffect(() => {
     setDatosColaborador({
-      nombre: "",
-      correo: "",
-      edad: "",
-      cargo: "",
-      telefono: ""
+      nombre: '',
+      correo: '',
+      edad: '',
+      cargo: '',
+      telefono: '',
     });
   }, [data]);
 
   return (
-    <div className='formulario col-12 col-lg-4'>
+    <div className="formulario col-12 col-lg-4">
       <h3>Agregar Colaborador</h3>
 
       <form noValidate onSubmit={(e) => validandoDatos(e)}>
-
         <div className="mb-3">
-          <input onChange={(e) => handlerInputs(e)} type="text" className="form-control" id="inputNombre" placeholder="Nombre del colaborador" />
+          <input
+            onChange={(e) => handlerInputs(e)}
+            type="text"
+            className="form-control"
+            id="inputNombre"
+            placeholder="Nombre del colaborador"
+          />
         </div>
 
         <div className="mb-3">
-          <input onChange={(e) => handlerInputs(e)} type="email" className="form-control" id="inputEmail" placeholder="Email del colaborador" pattern=".*" />
+          <input
+            onChange={(e) => handlerInputs(e)}
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            placeholder="Email del colaborador"
+            pattern=".*"
+          />
         </div>
 
         <div className="mb-3">
-          <input onChange={(e) => handlerInputs(e)} type="number" className="form-control" id="inputEdad" placeholder="Edad del colaborador" />
+          <input
+            onChange={(e) => handlerInputs(e)}
+            type="number"
+            className="form-control"
+            id="inputEdad"
+            placeholder="Edad del colaborador"
+          />
         </div>
 
         <div className="mb-3">
-          <input onChange={(e) => handlerInputs(e)} type="text" className="form-control" id="inputCargo" placeholder="Cargo del colaborador" />
+          <input
+            onChange={(e) => handlerInputs(e)}
+            type="text"
+            className="form-control"
+            id="inputCargo"
+            placeholder="Cargo del colaborador"
+          />
         </div>
 
         <div className="mb-3">
-          <input onChange={(e) => handlerInputs(e)} type="text" className="form-control" id="inputTelefono" placeholder="Teléfono del colaborador" />
+          <input
+            onChange={(e) => handlerInputs(e)}
+            type="text"
+            className="form-control"
+            id="inputTelefono"
+            placeholder="Teléfono del colaborador"
+          />
         </div>
 
         <div className="d-grid mb-3">
-          <button type="submit" className="btn">Agregar Colaborador</button>
+          <button type="submit" className="btn">
+            Agregar Colaborador
+          </button>
         </div>
-
       </form>
-
     </div>
-  )
+  );
 }
